5b872616aa5bb22a323b463cc5c45c2555fd08c7
Businge931 <jimmybusinge9@gmail.com>
2024-09-09 21:41:11 +0300

    feat: show computer canvas

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -1,9 +1,110 @@
-import React from 'react'
+import React from "react";
+import { Suspense, useEffect, useState } from "react";
 
-const Computers = () => {
+import { Canvas, useLoader } from "@react-three/fiber";
+import { OrbitControls, Preload, useGLTF } from "@react-three/drei";
+import CanvasLoader from "../Loader";
+
+//
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
+
+const Computers = ({ isMobile }) => {
+  // const computer = useGLTF("./desktop_pc/scene.gltf");
+  const gltf = useLoader(GLTFLoader, "./Poimandres.gltf");
+
   return (
-    <div>Computers</div>
-  )
-}
+    <mesh>
+      <hemisphereLight intensity={0.15} groundColor="black" />
+      <pointLight intensity={1} />
+      <spotLight
+        position={[-20, 50, 10]}
+        angle={0.12}
+        penumbra={1}
+        intensity={1}
+        castShadow
+        shadow-mapSize={1024}
+      />
+      <primitive
+        object={gltf.scene}
+        scale={isMobile ? 0.7 : 0.75}
+        position={isMobile ? [0, -3, -2.2] : [0, -3.25, -1.5]}
+        rotation={[-0.01, -0.2, -0.1]}
+      />
+    </mesh>
+  );
+};
 
-export default Computers
+const ComputersCanvas = () => {
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    // Add a listener for changes to the screen size
+    const mediaQuery = window.matchMedia("(max-width: 500px)");
+
+    // Set the initial value of the `isMobile` state variable
+    setIsMobile(mediaQuery.matches);
+
+    // Define a callback function to handle changes to the media query
+    const handleMediaQueryChange = (event) => {
+      setIsMobile(event.matches);
+    };
+
+    // Add the callback function as a listener for changes to the media query
+    mediaQuery.addEventListener("change", handleMediaQueryChange);
+
+    // Remove the listener when the component is unmounted
+    return () => {
+      mediaQuery.removeEventListener("change", handleMediaQueryChange);
+    };
+  }, []);
+
+  return (
+    <Canvas
+      frameloop="demand"
+      shadows
+      camera={{ position: [20, 3, 5], fov: 25 }}
+      gl={{ preserveDrawingBuffer: true }}
+    >
+      <Suspense fallback={<CanvasLoader />}>
+        <OrbitControls
+          enableZoom={false}
+          maxPolarAngle={Math.PI / 2}
+          minPolarAngle={Math.PI / 2}
+        />
+        <Computers isMobile={isMobile} />
+      </Suspense>
+      <Preload all />
+    </Canvas>
+  );
+};
+
+export default ComputersCanvas;
+
+// import { Canvas } from "@react-three/fiber";
+// import { useLoader } from "@react-three/fiber";
+// import { Environment, OrbitControls } from "@react-three/drei";
+// import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
+// import { Suspense } from "react";
+
+// const Model = () => {
+//   const gltf = useLoader(GLTFLoader, "./Poimandres.gltf");
+//   return (
+//     <>
+//       <primitive object={gltf.scene} scale={0.4} />
+//     </>
+//   );
+// };
+
+// export default function App() {
+//   return (
+//     <div className="App">
+//       <Canvas>
+//         <Suspense fallback={null}>
+//           <Model />
+//           <OrbitControls />
+//           <Environment preset="sunset" background />
+//         </Suspense>
+//       </Canvas>
+//     </div>
+//   );
+// }
